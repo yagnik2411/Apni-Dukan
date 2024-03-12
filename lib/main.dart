8be38e2c20d9f8c32f86ddc76d73c6922a3b0753
yagnik panchal <panchalyagnik2411@gmail.com>
2024-03-12 22:09:46 +0530

    firebase intilized

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,36 @@
+import 'package:apni_dukan/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'features/home/ui/home.dart';
 
-void main() {
-  runApp( MyApp());
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
-   MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  ScreenUtilInit(
-      designSize:  const Size(393, 873),
+    return ScreenUtilInit(
+      designSize: const Size(393, 873),
       child: MaterialApp(
-      title: "Apni Dukan",
+        title: "Apni Dukan",
         // ignore: unnecessary_new
         theme: ThemeData(
-          backgroundColor: Colors.white,
-          appBarTheme: AppBarTheme(
-            backgroundColor: Colors.teal.shade300,
-            foregroundColor: Colors.white,
-            // centerTitle: true
-          )
-        ),
+            backgroundColor: Colors.white,
+            appBarTheme: AppBarTheme(
+              backgroundColor: Colors.teal.shade300,
+              foregroundColor: Colors.white,
+            )),
         debugShowCheckedModeBanner: false,
         home: const Home(),
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
