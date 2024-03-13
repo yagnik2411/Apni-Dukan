e0f45a06f3ae935bf45fd525b6179a905ea65947
yagnik panchal <panchalyagnik2411@gmail.com>
2024-03-13 15:03:40 +0530

    bloc imnlized

--- lib/features/Login/ui/login_options.dart
+++ lib/features/Login/ui/login_options.dart
@@ -1,138 +1,41 @@
+import 'package:apni_dukan/features/Login/bloc/login_bloc.dart';
+import 'package:apni_dukan/features/Login/ui/widget/login_page.dart';
 import 'package:apni_dukan/features/Login/ui/widget/options_page.dart';
+import 'package:apni_dukan/features/Login/ui/widget/register_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class LoginRegisterOptionPage extends StatelessWidget {
+class LoginRegisterOptionPage extends StatefulWidget {
   const LoginRegisterOptionPage({super.key});
 
   @override
+  State<LoginRegisterOptionPage> createState() =>
+      _LoginRegisterOptionPageState();
+}
+
+class _LoginRegisterOptionPageState extends State<LoginRegisterOptionPage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.teal.shade100,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            100.verticalSpace,
-            Container(
-              alignment: Alignment.center,
-              width: 200.w,
-              height: 70.h,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.grey.shade200,
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.black12,
-                      blurRadius: 10,
-                      spreadRadius: 5,
-                      offset: Offset(0, 5),
-                    ),
-                  ]),
-              child: Text(
-                "Login",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 35.sp,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.teal.shade300,
-                ),
-              ),
+    return BlocConsumer<LoginBloc, LoginState>(
+      listener: (context, state) {
+        // TODO: implement listener
+      },
+      builder: (context, state) {
+        return Container(
+          alignment: Alignment.center,
+          color: Colors.white,
+          width: double.infinity,
+          height: double.infinity,
+          child: SizedBox(
+            height: 80.w,
+            width: 80.w,
+            child: CircularProgressIndicator(
+              color: Colors.teal.shade300,
             ),
-            200.verticalSpace,
-            Padding(
-              padding:  EdgeInsets.all(10.0.w),
-              child: TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  hintText: "abc@xyz.com",
-                  hintStyle: TextStyle(
-                      color: Colors.teal.shade100,
-                      fontSize: 16.sp,
-                      fontWeight: FontWeight.w400),
-                  label: Container(
-                    padding:  EdgeInsets.all(10.sp),
-                    decoration:  const BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.all(Radius.circular(30))),
-                    child: Text(
-                      "Email",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16.sp,
-                          fontWeight: FontWeight.w400),
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                ),
-              ),
-            ),
-            20.verticalSpace,
-            Padding(
-              padding:  EdgeInsets.all(10.0.w),
-              child: TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  hintText: "abc123",
-                  hintStyle: TextStyle(
-                      color: Colors.teal.shade100,
-                      fontSize: 16.sp,
-                      fontWeight: FontWeight.w400),
-                  label: Container(
-                    padding:  EdgeInsets.all(10.sp),
-                    decoration:  const BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.all(Radius.circular(30))),
-                    child: Text(
-                      "Password",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16.sp,
-                          fontWeight: FontWeight.w400),
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                ),
-              ),
-            ),
-            
-            100.verticalSpace,
-            SizedBox(
-              width: 300.w,
-              height: 50.h,
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                ),
-                child: Text(
-                  "Login",
-                  style: TextStyle(
-                    fontSize: 20.sp,
-                    color: Colors.teal.shade400,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
